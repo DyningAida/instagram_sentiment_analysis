7e0cb496bba21135e4d3dba886ed60f73b53df9e
DyningAida <aida.batrishya@gmail.com>
2021-05-07 13:30:26 +0700

    add try except

--- app.py
+++ app.py
@@ -20,62 +20,65 @@ def index():
             "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.57",
             "cookie": "sessionid=2055545287%3AarnEjTWjTxeh7f%3A4;"
         }
-        # scrap data profil
-        ig.scrape(headers=headers)
-        # mengambil data recent post
-        ig_posts = ig.get_recent_posts()
-        posts = []
-        time = []
-        sentim = []
-        caption_list = []
-        num = []
-        polarity = []
-        i = 0
-        for post in ig_posts:
-            # scrap post
-            post.scrape(headers=headers)
-            # menambahkan data post yang discrap, mengubah ke dict dan append ke posts list
-            posts.append(post.to_dict())
-            # lower & clean data caption
-            caption = ' '.join(re.sub("(#[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\s+)", " ",str(posts[i]['caption']).lower()).split())
-            # memasukkan ke caption list
-            caption_list.append(caption)
-            # jika terdeteksi caption selain bahasa inggis, maka translate untuk cek polaritynya
-            if detect(str(caption)) != 'en':
-                # translate ke bahasa inggris
-                translator = google_translator()
-                eng_capt = translator.translate(caption,lang_tgt='en')
-                # blob caption
-                eng_capt = TextBlob(eng_capt)
-                # cek apakah panjang data > 3, jika tidak, maka menghasilkan no sentiment
-                if len(str(eng_capt))>3:
-                    if eng_capt.sentiment.polarity < 0:
-                        sentiment = 'negative'
-                    elif eng_capt.sentiment.polarity == 0:
-                        sentiment = 'neutral'
+        try:
+            # scrap data profil
+            ig.scrape(headers=headers)
+            # mengambil data recent post
+            ig_posts = ig.get_recent_posts()
+            posts = []
+            time = []
+            sentim = []
+            caption_list = []
+            num = []
+            polarity = []
+            i = 0
+            for post in ig_posts:
+                # scrap post
+                post.scrape(headers=headers)
+                # menambahkan data post yang discrap, mengubah ke dict dan append ke posts list
+                posts.append(post.to_dict())
+                # lower & clean data caption
+                caption = ' '.join(re.sub("(#[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\s+)", " ",str(posts[i]['caption']).lower()).split())
+                # memasukkan ke caption list
+                caption_list.append(caption)
+                # jika terdeteksi caption selain bahasa inggis, maka translate untuk cek polaritynya
+                if detect(str(caption)) != 'en':
+                    # translate ke bahasa inggris
+                    translator = google_translator()
+                    eng_capt = translator.translate(caption,lang_tgt='en')
+                    # blob caption
+                    eng_capt = TextBlob(eng_capt)
+                    # cek apakah panjang data > 3, jika tidak, maka menghasilkan no sentiment
+                    if len(str(eng_capt))>3:
+                        if eng_capt.sentiment.polarity < 0:
+                            sentiment = 'negative'
+                        elif eng_capt.sentiment.polarity == 0:
+                            sentiment = 'neutral'
+                        else:
+                            sentiment = 'positive'
                     else:
-                        sentiment = 'positive'
+                        sentiment = 'no sentiment'
                 else:
                     sentiment = 'no sentiment'
-            else:
-                sentiment = 'no sentiment'
-            # mengambil nilai polarity
-            polarity_value = eng_capt.sentiment.polarity
-            polarity.append(polarity_value)
-            # mengambil waktu upload
-            upload_time = posts[i]['upload_date']
-            time.append(upload_time)
-            # memasukkan sentiment
-            sentim.append(sentiment)
-            i += 1
-        for x in range(len(posts)):
-            # untuk mengurutkan data
-            num.append(x+1)
-        # total data
-        amount_data = len(posts)
-        # satukan data yang dibutuhkan      
-        list_data = list(zip(num, time, caption_list, polarity, sentim))
-        return render_template('index.html', list_data=list_data, amount_data=amount_data, username=username)
+                # mengambil nilai polarity
+                polarity_value = eng_capt.sentiment.polarity
+                polarity.append(polarity_value)
+                # mengambil waktu upload
+                upload_time = posts[i]['upload_date']
+                time.append(upload_time)
+                # memasukkan sentiment
+                sentim.append(sentiment)
+                i += 1
+            for x in range(len(posts)):
+                # untuk mengurutkan data
+                num.append(x+1)
+            # total data
+            amount_data = len(posts)
+            # satukan data yang dibutuhkan      
+            list_data = list(zip(num, time, caption_list, polarity, sentim))
+            return render_template('index.html', list_data=list_data, amount_data=amount_data, username=username)
+        except:
+            return render_template('none.html')
     return render_template('layout.html')
 #if __name__ == "__main__":
 #    app.run(debug=True)
